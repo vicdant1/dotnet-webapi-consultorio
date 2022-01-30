f41a6f8159ef24b38774ee186e957d567b746acc
vicdant1 <dantjv@outlook.com>
2022-01-30 20:32:45 -0300

    setting override to paciente table

---
+++ Consultorio/Context/MappingFluentAPI/PacienteMapping.cs
@@ -0,0 +1,24 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Consultorio.Context.MappingFluentAPI
+{
+    public class PacienteMapping : BaseMap<Paciente>
+    {
+        public PacienteMapping() : base("tb_paciente")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<Paciente> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.Nome).HasColumnName("nome").HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.Cpf).HasColumnName("cpf").HasColumnType("varchar(11)").IsRequired();
+            builder.Property(p => p.Email).HasColumnName("email").HasColumnType("varchar(50)");
+            builder.Property(p => p.Celular).HasColumnName("celular").HasColumnType("varchar(100)").IsRequired();
+
+        }
+    }
+}
