2418f469688ba98deb7dcf6cca4a454f0b1e8f0d
vicdant1 <dantjv@outlook.com>
2022-01-30 20:34:56 -0300

    setting override to profissional table

---
+++ Consultorio/Context/MappingFluentAPI/ProfissionalMapping.cs
@@ -0,0 +1,22 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Consultorio.Context.MappingFluentAPI
+{
+    public class ProfissionalMapping : BaseMap<Profissional>
+    {
+        public ProfissionalMapping() : base("tb_profissional")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<Profissional> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Nome).HasColumnName("nome").HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Ativo).HasColumnName("ativo");
+
+        }
+    }
+}
