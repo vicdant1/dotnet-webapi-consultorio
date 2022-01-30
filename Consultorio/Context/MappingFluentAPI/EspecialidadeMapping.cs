16ddb811ff0277a254acdc921b0c4e301a414de2
vicdant1 <dantjv@outlook.com>
2022-01-30 20:26:15 -0300

    setting override to especialidade table

---
+++ Consultorio/Context/MappingFluentAPI/EspecialidadeMapping.cs
@@ -0,0 +1,20 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Consultorio.Context.MappingFluentAPI
+{
+    public class EspecialidadeMapping : BaseMap<Especialidade>
+    {
+        public EspecialidadeMapping() : base("tb_especialidade")
+        { }
+
+        public override void Configure(EntityTypeBuilder<Especialidade> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(e => e.Nome).HasColumnName("nome").IsRequired();
+            builder.Property(e => e.Ativa).HasColumnName("ativa").HasDefaultValue(true);
+        }
+    }
+}
