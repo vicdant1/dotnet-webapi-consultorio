6e3394c69725d006a86faa3fe057a0ac9c08193e
vicdant1 <dantjv@outlook.com>
2022-01-30 20:36:44 -0300

    Creating database migrations

---
+++ Consultorio/Migrations/20220130233552_addNewTableConstraints.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Consultorio.Migrations
+{
+    public partial class addNewTableConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_especialidade",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ativa = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_especialidade", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_paciente",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    celular = table.Column<string>(type: "varchar(100)", nullable: false),
+                    cpf = table.Column<string>(type: "varchar(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_paciente", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_profissional",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_profissional", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_especialidade");
+
+            migrationBuilder.DropTable(
+                name: "tb_paciente");
+
+            migrationBuilder.DropTable(
+                name: "tb_profissional");
+        }
+    }
+}
